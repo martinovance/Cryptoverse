0e197bd0b2f5b471c1963278848f231188d4fb44
Martins Ogunsina <martinsogunsina@gmail.com>
2022-05-24 12:54:57 +0100

    update commit

---
+++ src/Services/cryptoApi.js
@@ -0,0 +1,31 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+    'X-RapidAPI-Key': '37eef9066dmshc6adc3ffcd31566p118ccajsnb49b833c526f'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`),
+        }),
+        getExchanges: builder.query({
+            query: () => createRequest('/exchanges'),
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`),
+        }),
+        getCryptoHistory: builder.query({
+            query: ({ coinId, timeperiod }) => createRequest(`/coin/${coinId}/history/${timeperiod}`),
+        }),
+    })
+});
+
+export const { useGetCryptosQuery, useGetExchangesQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } = cryptoApi;
---
+++ src/components/News.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
+import moment from 'moment';
+
+import { useGetCryptoNewsQuery } from '../Services/cryptoNewsApi';
+import { useGetCryptosQuery } from '../Services/cryptoApi';
+import Loader from './Loader';
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
+
+const News = ({ simplified }) => {
+  const [newsCategory, setNewsCategory] = useState('Cryptocurrency')
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
+  const { data } = useGetCryptosQuery(100);
+
+  if(!cryptoNews?.value) return <Loader />;
+
+  return (
+    <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            classNme="select-news"
+            placeholder="Select a Crpto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            fillterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0 }
+          >
+              <Option value="Cryptocurrency">Cryptocurrency</Option>
+              {data?.data?.coins.map((coin) => <Option value={coin.name}>{coin.name}</Option> )}
+          </Select>
+        </Col>
+      )}
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={12} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>{news.name}</Title>
+                <img style={{ maxWidth: '200px', maxHeight: '100px' }} src={ news?.image?.thumbnails?.contentUrl || demoImage } alt="" />
+              </div>
+              <p>
+                {news.description.length > 100 
+                ? `${news.description.substring(0, 100)}...`
+                :news.description
+                }
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage}  alt="" />
+                  <Text className="provider-name">{news.provider[0]?.name}</Text>
+                </div>
+                  <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
+              </div>
+            </a>
+           </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+};
+
+export default News;
